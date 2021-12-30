6e0f426c5d9c4e1d638e195dbcbcce8ff518f366
Terry Tumen <turuut1@gmail.com>
2021-12-30 04:55:52 -0600

    Initial Commit

--- main.py
+++ main.py
@@ -12,4 +12,6 @@ client = commands.Bot(command_prefix="./", intents = discord.Intents.all())
 for i in range(len(cogs)):
   cogs[i].setup(client)
 
-client.run("OTI2MDIyNTU0OTE1ODM1OTA0.Yc1ntQ.4Zxv_ZiV0PbGsF1nXalR5lRAB28")
+#your own token
+client.run("")
+
